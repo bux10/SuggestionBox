4985d653b0b79d99c5fe4a5a90148e23e5867edf
Dustin Moore <Dustin Moore>
2017-03-19 16:27:33 -0600

    first commit

---
+++ js/test.js
@@ -0,0 +1,34 @@
+app.directive('appInfo', function() { 
+  return { 
+    restrict: 'E', 
+    scope: { 
+      info: '=' 
+    }, 
+    templateUrl: 'js/directives/appInfo.html' 
+  }; 
+});
+
+app.factory('forecast', ['$http', function($http) { 
+  return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json') 
+            .success(function(data) { 
+              return data; 
+            }) 
+            .error(function(err) { 
+              return err; 
+            }); 
+}]);
+
+app.config(function ($routeProvider) { 
+  $routeProvider 
+    .when('/', { 
+      controller: 'HomeController', 
+      templateUrl: 'views/home.html' 
+    }) 
+    .otherwise({ 
+      redirectTo: '/' 
+    }); 
+});
+
+app.controller('MainController', ['$scope', function($scope) { 
+  $scope.title = 'Top Sellers in Books'; 
+}]);
