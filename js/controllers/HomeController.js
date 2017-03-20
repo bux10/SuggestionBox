5d8d03e2011c9f5cbcc3a2e9cc15ce53580dfa4c
Dustin Moore <dmoore@frostbytecafe.com>
2017-03-20 14:33:03 -0600

    3/20/17 2:32

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,4 +1,19 @@
-app.controller('HomeController', ['$scope', 'suggestions', function($scope, suggestions){
+app.controller('HomeController', ['$scope', 'suggestions', function ($scope, suggestions) {
     $scope.posts = suggestions.posts;
-    // console.log($scope.posts);
+
+    $scope.upVote = function (post) {
+        post.upvotes += 1;
+    };
+
+    $scope.addSuggestion = function () {
+        if (!$scope.title || $scope.title === "") {
+            return;
+        }
+        $scope.posts.push({
+            title: $scope.title,
+            upvotes: 0
+        });
+        $scope.title = '';
+    }
+
 }]);
